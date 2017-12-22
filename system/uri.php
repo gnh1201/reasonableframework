e83d2d1fce5133a89239570d265838c7802d2eb6
Namhyeon, Go <gnh1201@gmail.com>
2017-12-23 03:41:14 +0900

    Update uri.php

--- system/uri.php
+++ system/uri.php
@@ -2,12 +2,23 @@
 if(!function_exists("base_url")) {
 	function base_url() {
 		global $config;
-		
+
 		$base_url = '';
 		if(array_key_exists("base_url", $config)) {
 			$base_url = $config["base_url"];
 		}
 
 		return $base_url;
+	}
+}
+
+if(!function_exists("get_uri")) {
+	function get_uri() {
+		$request_uri = '';
+		if(array_key_exists("REQUEST_URI", $_SERVER)) {
+			$request_uri = $_SERVER["REQUEST_URI"];
+		}
+
+		return $request_uri;
 	}
 }
