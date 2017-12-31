b3b560356d37a1eafc6e341d3ef6ce00414f8bec
Namhyeon, Go <gnh1201@gmail.com>
2017-12-31 23:50:25 +0900

    Update database.php

--- system/database.php
+++ system/database.php
@@ -10,3 +10,18 @@ $conn = new PDO(
 	array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 );
 $conn->query("SET NAMES 'utf8'");
+
+if(!function_exists("sql_query")) {
+	function sql_query($sql, $bind=array()) {
+		global $conn;
+
+		$stmt = $conn->prepare($sql);
+		if(count($bind) > 0) {
+			foreach($bind as $k=>$v) {
+				$stmt->bindParam(':' . $k, $v);
+			}
+		}
+
+		return $stmt->execute();
+	}
+}
