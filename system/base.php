e81f5a2485eed2f246472bcac3c824e5565e56ce
Namhyeon, Go <gnh1201@gmail.com>
2018-01-01 02:00:50 +0900

    Update base.php

--- system/base.php
+++ system/base.php
@@ -14,3 +14,18 @@ if(function_exists('array_key_empty')) {
 		return $empty;
 	}
 }
+
+if(!function_exists("cut_str")) {
+	function cut_str($str, $start, $len=0) {
+		$cutted_str = "";
+		if(function_exists("iconv_substr")) {
+			$cutted_str = iconv_substr($str, $start, $len, "utf-8");
+		} elseif(function_exists("mb_substr")) {
+			$cutted_str = mb_substr($str, $start, $len);
+		} else {
+			$cutted_str = substr($start, $len);
+		}
+		
+		return $cutted_str;
+	}
+}
