081bac3ea811848b4d65f4568bd1842ff3e1f84a
Namhyeon, Go <gnh1201@gmail.com>
2018-02-09 18:03:06 +0900

    Update security.php

--- system/security.php
+++ system/security.php
@@ -18,3 +18,30 @@ if(!function_exists("check_token_abuse")) {
 		return $abuse;
 	}
 }
+
+if(!function_exists("make_random_id")) {
+	function make_random_id($length = 10) {
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$charactersLength = strlen($characters);
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, $charactersLength - 1)];
+		}
+		return $randomString;
+	}
+}
+
+if(!function_exists("set_session_token")) {
+	function set_session_token() {
+		$random_id = make_random_id(10);
+		$_SESSION['random_id'] = $random_id;
+
+		return $random_id;
+	}
+}
+
+if(!function_exists("get_session_token")) {
+	function get_session_token() {
+		return $_SESSION['random_id'];
+	}
+}
