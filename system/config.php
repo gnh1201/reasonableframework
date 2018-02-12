e266e4a6eceb9796b76e580bf6da1d04e05575ff
Namhyeon, Go <gnh1201@gmail.com>
2018-02-12 14:43:11 +0900

    Update config.php

--- system/config.php
+++ system/config.php
@@ -1,13 +1,21 @@
 <?php
-$config = array();
-if($handle = opendir('./config')) {
-    while (false !== ($file = readdir($handle))) {
-        if ($file != "." && $file != ".." && end(explode('.', $file)) == 'ini') {
-            $ini = parse_ini_file('./config/' . $file);
-			foreach($ini as $k=>$v) {
-				$config[$k] = $v;
-			}
+if(!function_exists("get_config")) {
+    function get_config() {
+        $config = array();
+        if($handle = opendir('./config')) {
+            while (false !== ($file = readdir($handle))) {
+                if ($file != "." && $file != ".." && end(explode('.', $file)) == 'ini') {
+                    $ini = parse_ini_file('./config/' . $file);
+                    foreach($ini as $k=>$v) {
+                        $config[$k] = $v;
+                    }
+                }
+            }
+            closedir($handle);
         }
+        
+        return $config;
     }
-    closedir($handle);
 }
+
+$config = get_config();
