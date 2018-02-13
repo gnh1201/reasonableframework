efb8c0dda7120e6d703200aff8c7a6cd9b49d3c1
Namhyeon, Go <gnh1201@gmail.com>
2018-02-13 17:07:49 +0900

    Update database.php

--- system/database.php
+++ system/database.php
@@ -1,86 +1,112 @@
 <?php
-$conn = new PDO(
-	sprintf(
-		"mysql:host=%s;dbname=%s;charset=utf8",
-		$config['db_host'],
-		$config['db_name']
-	),
-	$config['db_username'],
-	$config['db_password'],
-	array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-);
-$conn->query("SET NAMES 'utf8'");
+if(!function_exists("get_db_connect")) {
+	function get_db_connect() {
+		global $config;
 
-if(!function_exists("sql_query")) {
-	function sql_query($sql, $bind=array()) {
-		return get_db_stmt($sql, $bind);
+		$conn = new PDO(
+			sprintf(
+				"mysql:host=%s;dbname=%s;charset=utf8",
+				$config['db_host'],
+				$config['db_name']
+			),
+			$config['db_username'],
+			$config['db_password'],
+			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+		);
+		$conn->query("SET NAMES 'utf8'");
+		
+		return $conn;
 	}
 }
 
-function get_dbc_object() {
-	global $dbc;
-	return $dbc;
+if(!function_exists("exec_stmt_query")) {
+	function exec_stmt_query($sql, $bind=array()) {
+		$stmt = get_db_stmt($sql, $bind);
+		$stmt->execute();
+
+		return $stmt;
+	}
 }
 
-function get_db_stmt($sql, $bind=array()) {
-	$stmt = get_dbc_object()->prepare($sql);
-	if(count($bind) > 0) {
-		foreach($bind as $k=>$v) {
-			$stmt->bindParam(':' . $k, $v);
+if(!function_exists("get_dbc_object")) {
+	function get_dbc_object($renew=false) {
+		global $dbc;
+
+		if($renew) {
+			$dbc = get_db_connect();
 		}
+
+		return $dbc;
 	}
-	return $stmt;
 }
 
-function get_db_last_id() {
-	return get_dbc_object()->lastInsertId();
+if(!function_exists("get_db_stmt")) {
+	function get_db_stmt($sql, $bind=array()) {
+		$stmt = get_dbc_object()->prepare($sql);
+		if(count($bind) > 0) {
+			foreach($bind as $k=>$v) {
+				$stmt->bindParam(':' . $k, $v);
+			}
+		}
+		return $stmt;
+	}
 }
 
-function exec_db_query($sql, $bind=array(), $options=array()) {
-	$dbc = get_dbc_object();
+if(!function_exists("get_db_last_id")) {
+	function get_db_last_id() {
+		return get_dbc_object()->lastInsertId();
+	}
+}
 
-	$flag = false;
-	$stmt = get_db_stmt($sql, $bind);
+if(!function_exists("exec_db_query")) {
+	function exec_db_query($sql, $bind=array(), $options=array()) {
+		$dbc = get_dbc_object();
 
-	$validOptions = array();
-	$optionAvailables = array("is_check_count", "is_commit");
-	foreach($optionAvailables as $opt) {
-		if(!array_key_empty($opt, $options)) {
-			$validOptions[$opt] = $options[$opt];
+		$flag = false;
+		$stmt = get_db_stmt($sql, $bind);
+
+		$validOptions = array();
+		$optionAvailables = array("is_check_count", "is_commit");
+		foreach($optionAvailables as $opt) {
+			if(!array_key_empty($opt, $options)) {
+				$validOptions[$opt] = $options[$opt];
+			} else {
+				$validOptions[$opt] = false;
+			}
+		}
+		extract($validOptions);
+		
+		if($is_commit) {
+			$dbc->beginTransaction();
+		}
+
+		if($is_check_count == true) {
+			if($stmt->execute() && $stmt->rowCount() > 0) {
+				$flag = true;
+			}
 		} else {
-			$validOptions[$opt] = false;
+			$flag = $stmt->execute();
 		}
-	}
-	extract($validOptions);
-	
-	if($is_commit) {
-		$dbc->beginTransaction();
-	}
 
-	if($is_check_count == true) {
-		if($stmt->execute() && $stmt->rowCount() > 0) {
-			$flag = true;
+		if($is_commit) {
+			$dbc->commit();
 		}
-	} else {
-		$flag = $stmt->execute();
-	}
 
-	if($is_commit) {
-		$dbc->commit();
+		return $flag;
 	}
-
-	return $flag;
 }
 
-function exec_db_fetch_all($sql, $bind=array()) {
-	$rows = array();
-	$stmt = get_db_stmt($sql, $bind);
-	
-	if($stmt->execute() && $stmt->rowCount() > 0) {
-		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if(!function_exists("exec_db_fetch_all")) {
+	function exec_db_fetch_all($sql, $bind=array()) {
+		$rows = array();
+		$stmt = get_db_stmt($sql, $bind);
+		
+		if($stmt->execute() && $stmt->rowCount() > 0) {
+			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
+		
+		return $rows;
 	}
-	
-	return $rows;
 }
 
 // set global db connection variable
