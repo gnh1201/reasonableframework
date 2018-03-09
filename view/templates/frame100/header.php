e6e627b5ea27f3efad87465544e1079d01dadffd
Namhyeon, Go <gnh1201@gmail.com>
2018-03-09 21:21:05 +0900

    Update header.php

--- view/templates/frame100/header.php
+++ view/templates/frame100/header.php
@@ -1,7 +1,7 @@
 !DOCTYPE HTML>
 <html>
         <head>      
-                <title>Reasonable Frame100</title>
+                <title>ReasonableFramework Frame100</title>
                 <link rel="stylesheet" type="text/css" href="./view/templates/frame100/assets/css/style.css">
         </head>
         <body>
