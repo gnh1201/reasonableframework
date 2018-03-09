61d0bc73ab332883189879f8840b41e6aea1a72a
Namhyeon, Go <gnh1201@gmail.com>
2018-03-09 21:19:55 +0900

    Create header.php

---
+++ view/templates/frame100/header.php
@@ -0,0 +1,7 @@
+!DOCTYPE HTML>
+<html>
+        <head>      
+                <title>BMCNET IDM</title>
+                <link rel="stylesheet" type="text/css" href="./view/templates/frame100/assets/css/style.css">
+        </head>
+        <body>
