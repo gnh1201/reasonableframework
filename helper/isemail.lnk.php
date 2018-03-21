ff48cb9af1b50e224a7bc7ae1c4783a7d697ea2e
Namhyeon, Go <gnh1201@gmail.com>
2018-03-21 11:10:51 +0900

    Create isemail.lnk.php

---
+++ helper/isemail.lnk.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file isemail.lnk.php
+ * @date 2018-03-02
+ * @author Go Namhyeon <gnh1201@gmail.com>
+ * @brief IsEmail library linker
+ */
+
+$inc_file = "./vendor/isemail/is_email.php";
+if(file_exists($inc_file)) {
+	include($inc_file);
+}
