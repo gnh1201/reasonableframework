dc6a2c6a5c00c493b9406b109e34e4f2f78eabf6
Namhyeon, Go <gnh1201@gmail.com>
2018-03-21 11:25:20 +0900

    Update gnuboard.config.php

--- helper/gnuboard.config.php
+++ helper/gnuboard.config.php
@@ -45,7 +45,7 @@ $g4['visit_table']         = $g4['table_prefix'] . "visit";         // 방문자
 $g4['visit_sum_table']     = $g4['table_prefix'] . "visit_sum";     // 방문자 합계 테이블
 $g4['token_table']         = $g4['table_prefix'] . "token";         // 토큰 테이블
 
-$g4['cookie_domain'] = ".power.386.org";
+//$g4['cookie_domain'] = ".power.386.org";
 
 $g4['link_count'] = 2;
 $g4['charset'] = "utf-8";
