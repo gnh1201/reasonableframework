8293d849b029298d4eadb31f2d387ebc4cecd6ed
Namhyeon, Go <gnh1201@gmail.com>
2018-04-15 00:03:12 +0900

    Update gnuboard.dbt.php

--- helper/gnuboard.dbt.php
+++ helper/gnuboard.dbt.php
@@ -80,11 +80,11 @@ if(!function_exists("gnb_write_post")) {
         );
 
         foreach($write_default_fields as $k=>$v) {
-			if(in_array($k, array("mb_id", "wr_num"))) {
-				$write_fields[$k] = $v;
-			} else {
-				$write_fields[$k] = array_key_empty($k, $data) ? $v : $data[$k];
-			}
+            if(in_array($k, array("mb_id", "wr_num"))) {
+                $write_fields[$k] = $v;
+            } else {
+                $write_fields[$k] = array_key_empty($k, $data) ? $v : $data[$k];
+            }
         }
 
         if(count($write_fields) > 0) {
@@ -227,11 +227,11 @@ if(!function_exists("gnb_join_member")) {
         );
 
         foreach($member_default_fields as $k=>$v) {
-			if(in_array($k, array("mb_id", "mb_password"))) {
-				$member_fields[$k] = $v;
-			} else {
-				$member_fields[$k] = array_key_empty($k, $data) ? $v : $data[$k];
-			}
+            if(in_array($k, array("mb_id", "mb_password"))) {
+                $member_fields[$k] = $v;
+            } else {
+                $member_fields[$k] = array_key_empty($k, $data) ? $v : $data[$k];
+            }
         }
 
         if(count($member_fields) > 0) {
