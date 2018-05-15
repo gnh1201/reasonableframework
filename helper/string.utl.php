02463519b4bda33d19f60a7c7263da6c0924cbee
Namhyeon, Go <gnh1201@gmail.com>
2018-05-16 03:19:27 +0900

    Create string.utl.php

---
+++ helper/string.utl.php
@@ -0,0 +1,15 @@
+<?php
+function add_hyphen($tel) {
+    $tel = preg_replace("/[^0-9]/", "", $tel); // 숫자 이외 제거
+    if (substr($tel,0,2)=='02')
+        return preg_replace("/([0-9]{2})([0-9]{3,4})([0-9]{4})$/", "\\1-\\2-\\3", $tel);
+    else if (strlen($tel)=='8' && (substr($tel,0,2)=='15' || substr($tel,0,2)=='16' || substr($tel,0,2)=='18'))
+        // 지능망 번호이면
+        return preg_replace("/([0-9]{4})([0-9]{4})$/", "\\1-\\2", $tel);
+    else
+        return preg_replace("/([0-9]{3})([0-9]{3,4})([0-9]{4})$/", "\\1-\\2-\\3", $tel);
+}
+
+function br2nl($string) {
+    return preg_replace('/\<br(\s*)?\/?\>/i', "\n", $string); 
+}
