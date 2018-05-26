40e85fcd0cc2071c755c49890702e5034f89beb3
Namhyeon, Go <gnh1201@gmail.com>
2018-05-27 02:30:56 +0900

    Update and rename audit.php to logger.php

---
+++ system/logger.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file logger.php
+ * @date 2018-05-27
+ * @author Go Namhyeon <gnh1201@gmail.com>
+ * @brief Logger mobile for ReasonableFramework
+ */
+ 
+if(!function_exists("write_visit_log")) {
+	function write_visit_log() {
+		loadHelper("networktool");
+		$data = "\r\n" . json_encode(get_network_event());
+		return write_storage_file($data, array(
+			"stroage_type" => "logs",
+			"filename" => "vspf-" . date("Ymd"),
+			"mode" => "a",
+		));
+	}
+}
