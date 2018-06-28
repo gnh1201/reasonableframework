ef6740f412611e2b49d91287c90940e6e5bebb1e
Namhyeon, Go <gnh1201@gmail.com>
2018-06-28 17:22:05 +0900

    Update database.php

--- system/database.php
+++ system/database.php
@@ -274,23 +274,6 @@ if(!function_exists("get_bind_to_sql_update_set")) {
 	}
 }
 
-if(!function_exists("get_bind_to_sql_update_set")) {
-	// warning: variable k is not protected. do not use variable k and external variable without filter
-	function get_bind_to_sql_update_set($bind, $excludes=array()) {
-		$sql_update_set = "";
-		$set_items = "";
-
-		foreach($bind as $k=>$v) {
-			if(!in_array($k, $excludes)) {
-				$set_items[] = sprintf("%s = :%s", $k, $k);
-			}
-		}
-		$sql_update_set = implode(", ", $set_items);
-
-		return $sql_update_set;
-	}
-}
-
 if(!function_exists("get_bind_to_sql_select")) {
 	// warning: variable k is not protected. do not use variable k and external variable without filter
 	function get_bind_to_sql_select($tablename, $bind=array(), $options=array()) {
