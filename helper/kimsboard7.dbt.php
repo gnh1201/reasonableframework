00b02eaa8facb69885a4ad64eb0301dd497d1510
Namhyeon, Go <gnh1201@gmail.com>
2018-08-20 02:06:42 +0900

    Create kimsboard7.dbt.php

---
+++ helper/kimsboard7.dbt.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file kimsboard7.dbt.php
+ * @date 2018-08-20
+ * @author Go Namhyeon <gnh1201@gmail.com>
+ * @brief Database Helper for Kimsboard 7 (experimental)
+ */
+ 
+ // todo: write kimsboard interface
+ 
