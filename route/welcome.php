3c29eecf94aa5d4bda9d89aec2714b70004f5a61
Namhyeon, Go <gnh1201@gmail.com>
2018-08-25 00:01:10 +0900

    Update welcome.php

--- route/welcome.php
+++ route/welcome.php
@@ -1,3 +1,4 @@
 <?php
-if(!defined("_DEF_VSPF_")) exit("Do not allowed your access");
-renderView('welcome');
+if(!defined("_DEF_RSF_")) set_error_exit("do not allow access");
+
+renderView("view_welcome");
