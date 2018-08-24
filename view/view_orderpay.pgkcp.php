4f17f3a13258ac0b1dda32b3227e98b33c5d1c03
Namhyeon, Go <gnh1201@gmail.com>
2018-08-25 01:44:00 +0900

    Create view_orderpay.pgkcp.php

---
+++ view/view_orderpay.pgkcp.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file view_orderpay.pgkcp.php
+ * @date 2018-08-25
+ * @author Go Namhyeon <gnh1201@gmail.com>
+ * @brief KCP PG(Payment Gateway) View
+ */
+
+if(!defined("_DEF_RSF_")) set_error_exit("do not allow access");
+
+echo "<pre>";
+var_dump($data);
+echo "</pre>";
