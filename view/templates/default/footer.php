4a21ef9bbe02f96c3111139b7e8f560b5557f13f
Namhyeon, Go <gnh1201@gmail.com>
2018-08-25 00:01:51 +0900

    Update footer.php

--- view/templates/default/footer.php
+++ view/templates/default/footer.php
@@ -1,5 +1,5 @@
 <?php
-if(!defined("_DEF_VSPF_")) exit("Do not allowed your access");
+if(!defined("_DEF_RSF_")) set_error_exit("do not allow access");
 ?>
 <p>This is footer</p>
 <p>http://github.com/gnh1201/verysimplephpframework</p>
