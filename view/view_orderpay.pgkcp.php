83e1d5aa3f7572646ba6e901dc417b9f5223c77c
Namhyeon, Go <gnh1201@gmail.com>
2018-08-27 02:27:42 +0900

    Update view_orderpay.pgkcp.php

--- view/view_orderpay.pgkcp.php
+++ view/view_orderpay.pgkcp.php
@@ -7,7 +7,40 @@
  */
 
 if(!defined("_DEF_RSF_")) set_error_exit("do not allow access");
+?>
 
-echo "<pre>";
-var_dump($data);
-echo "</pre>";
+<!DOCTYPE html>
+<html>
+<head>
+	<title>*** NHN KCP [AX-HUB Version] ***</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
+	<meta http-equiv="Pragma" content="no-cache"> 
+	<meta http-equiv="Expires" content="-1">
+</head>
+
+<body>
+	<h1>NHN KCP [AX-HUB Version]</h1>
+
+	<form id="order_info" name="order_info" method="post" action="<?php echo $pgkcp_action_url; ?>">
+		<div>
+			<ul>
+<?php
+				foreach($payinfo as $k=>$v) {
+?>
+				<li>
+					<label for="<?php echo $k; ?>"><?php echo $k; ?></label>
+					<input id="<?php echo $k; ?>" name="<?php echo $k; ?>" value="<?php echo $v; ?>"/>
+				</li>
+<?php
+				}
+?>
+			</ul>
+
+			<button id="btn_submit" type="submit">Submit</button>
+		</div>
+	</form>
+
+	<?php echo $jsoutput; ?>
+</body>
+</html>
