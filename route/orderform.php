d0e6b94a646d3d0cede9f9f645f71828e415bfa3
Namhyeon, Go <gnh1201@gmail.com>
2018-09-03 02:29:44 +0900

    Create orderform.php

---
+++ route/orderform.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file orderform.php
+ * @date 2018-09-03
+ * @author Go Namhyeon <gnh1201@gmail.com>
+ * @brief order form
+ */
+
+if(!defined("_DEF_RSF_")) set_error_exit("do not allow access");
+
+set_session_token();
+
+$pay_method_alias = get_requested_value("pay_method_alias");
+
+$data = array(
+	"_token" => get_session_token(),
+	"_next_route" => "orderpay.pgkcp",
+	"redirect_url" => get_requested_value("redirect_url"),
+	"pay_method_alias" => $pay_method_alias,
+	"good_name" => get_requested_value("good_name"),
+	"good_mny" => get_requested_value("good_mny"),
+	"buyr_name" => get_requested_value("buyr_name"),
+	"buyr_mail" => get_requested_value("buyr_mail"),
+	"buyr_tel1" => get_requested_value("buyr_tel1"),
+);
+
+if(empty($pay_method_alias)) {
+	$data['pay_method_alias'] = "CRE";
+}
+
+renderView("view_orderform", $data);
