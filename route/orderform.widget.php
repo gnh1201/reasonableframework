a952c6d063dc61fad0b424cc9d4420a904925777
Namhyeon, Go <gnh1201@gmail.com>
2018-09-09 05:01:38 +0900

    Create orderform.widget.php

---
+++ route/orderform.widget.php
@@ -0,0 +1,21 @@
+<?php
+if(!defined("_DEF_RSF_")) set_error_exit("do not allow access");
+
+set_session_token();
+
+$pay_method_alias = get_requested_value("pay_method_alias");
+
+$data = array(
+	"_token" => get_session_token(),
+	"_next_route" => "orderpay.pgkcp",
+	"redirect_url" => get_requested_value("redirect_url"),
+	"pay_method_alias" => (!empty($pay_method_alias) ? $pay_method_alias : "CRE"),
+	"good_name" => get_requested_value("good_name"),
+	"good_mny" => get_requested_value("good_mny"),
+	"buyr_name" => get_requested_value("buyr_name"),
+	"buyr_mail" => get_requested_value("buyr_mail"),
+	"buyr_tel1" => get_requested_value("buyr_tel1"),
+	"pay_data" => get_requested_value("pay_data"),
+);
+
+renderView("view_orderform.widget", $data);
