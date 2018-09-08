1d62a7fa8a354c0a67e7e9fbb175fa107a0e30d2
Namhyeon, Go <gnh1201@gmail.com>
2018-09-09 05:02:16 +0900

    Update orderform.php

--- route/orderform.php
+++ route/orderform.php
@@ -16,16 +16,13 @@ $data = array(
 	"_token" => get_session_token(),
 	"_next_route" => "orderpay.pgkcp",
 	"redirect_url" => get_requested_value("redirect_url"),
-	"pay_method_alias" => $pay_method_alias,
+	"pay_method_alias" => (!empty($pay_method_alias) ? $pay_method_alias : "CRE"),
 	"good_name" => get_requested_value("good_name"),
 	"good_mny" => get_requested_value("good_mny"),
 	"buyr_name" => get_requested_value("buyr_name"),
 	"buyr_mail" => get_requested_value("buyr_mail"),
 	"buyr_tel1" => get_requested_value("buyr_tel1"),
+	"pay_data" => get_requested_value("pay_data"),
 );
-
-if(empty($pay_method_alias)) {
-	$data['pay_method_alias'] = "CRE";
-}
 
 renderView("view_orderform", $data);
