53f3f5cd3a4d7eb7206caf79598558675f899b8b
Namhyeon, Go <gnh1201@gmail.com>
2018-09-09 05:17:14 +0900

    Update view_payman.example.php

--- view/view_payman.example.php
+++ view/view_payman.example.php
@@ -1,19 +1,36 @@
-<script src="<?php echo base_url(); ?>/js/payman.js"></script>
-<script type="text/javascript">
-// load payman widget
-$(document).ready(function() {
-	var is_payman_loaded = payman_load_widget({
-		"pay_method_alias": "CRE",
-		"good_name": "채용정보등록",
-		"good_mny": 0,
-		"buyr_name": "<?php echo $name; ?>",
-		"buyr_mail": "<?php echo $email; ?>",
-		"buyr_tel1": "<?php echo $tel; ?>",
-		"pay_data": ""
-	});
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>payman example</title>
+	</head>
+	<body>
+		<div id="area_payman"></div>
 
-	if(is_payman_loaded == false) {
-		alert("결제를 중단합니다.");
-		document.location.href = "<?php echo $base_url; ?>";
-	}
-});
+		<script src="<?php echo base_url(); ?>/js/payman.js"></script>
+		<script type="text/javascript">
+		// load payman widget
+		$(document).ready(function() {
+			var is_payman_loaded = payman_load_widget({
+				"pay_method_alias": "CRE",
+				"good_name": "상품이름",
+				"good_mny": 0,
+				"buyr_name": "<?php echo $name; ?>",
+				"buyr_mail": "<?php echo $email; ?>",
+				"buyr_tel1": "<?php echo $tel; ?>",
+				"pay_data": ""
+			});
+
+			if(is_payman_loaded == false) {
+				alert("결제를 중단합니다.");
+				document.location.href = "<?php echo $base_url; ?>";
+			}
+
+			payman_set_data("good_mny", 1000);
+			payman_set_base64("pay_data", JSON.stringify({
+				"color": "pink",
+				"size": "2xl"
+			}));
+		});
+	</body>
+</html>
