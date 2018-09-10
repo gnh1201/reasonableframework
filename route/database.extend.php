ba75ca77b03fe5c34dae0ee831ef6a2f10fa4e30
Namhyeon, Go <gnh1201@gmail.com>
2018-09-10 14:50:53 +0900

    Create database.extend.php

---
+++ route/database.extend.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @file database.extend.php
+ * @date 2018-09-10
+ * @author Go Namhyeon <gnh1201@gmail.com>
+ * @brief Database Connection Extender
+ */
+
+if(function_exists("get_db_extend_connect")) {
+	function get_db_extend_connect($db_driver) {
+		$conn = false;
+
+		switch($db_driver) {
+			case "mysql.pdo":
+				// currently, mysql.pdo is default driver
+				break;
+
+			case "mysql.imp":
+				loadHelper("database.mysql.imp");
+				if(function_exists("get_db_mysql_imp_connect")) {
+					$conn = get_db_mysql_imp_connect();
+				}
+
+				break;
+			case "mysql.old":
+				loadHelper("database.mysql.old");
+				if(function_exists("get_db_mysql_old_connect")) {
+					$conn = get_db_mysql_old_connect();
+				}
+
+				break;
+
+			case "oracle":
+				loadHelper("database.oracle");
+				if(function_exists("get_db_oracle_connect")) {
+					$conn = get_db_oracle_connect();
+				}
+				break;
+		}
+
+		return $conn;
+	}
+}
