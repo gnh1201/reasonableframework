df6e26192409fa8363ff0f580d5823821d910ab1
Namhyeon, Go <gnh1201@gmail.com>
2018-09-26 19:26:45 +0900

    Update timetool.php

--- helper/timetool.php
+++ helper/timetool.php
@@ -29,25 +29,24 @@ if(!function_exists("query_time_server")) {
 }
 
 if(!function_exists("get_server_time")) {
-  function get_server_time($timeserver="time.bora.net", $dateformat="Y-m-d h:i:s") {
-    $datetime = false;
+  function get_server_time($timeserver="time.bora.net") {
+    $timestamp = false;
     $timercvd = query_time_server($timeserver, 37);
 
     //if no error from query_time_server
     if(!$timercvd[1]) {
         $timevalue = bin2hex($timercvd[0]);
         $timevalue = abs(HexDec('7fffffff') - HexDec($timevalue) - HexDec('7fffffff'));
-        $tmestamp = $timevalue - 2208988800; # convert to UNIX epoch time stamp
-        $datum = date("Y-m-d (D) H:i:s",$tmestamp - date("Z",$tmestamp)); /* incl time zone offset */
-        $doy = (date("z",$tmestamp)+1);
+        $timestamp = $timevalue - 2208988800; // convert to UNIX epoch time stamp
+        //$datum = date("Y-m-d (D) H:i:s", $tmestamp - date("Z", $timestamp)); // incl time zone offset
+        //$doy = (date("z", $tmestamp) + 1);
 
-        //echo "Time check from time server ",$timeserver," : [<font color=\"red\">",$timevalue,"</font>]";
+        //echo "Time check from time server ", $timeserver, " : [<font color=\"red\">",$timevalue,"</font>]";
         //echo " (seconds since 1900-01-01 00:00.00).<br>\n";
         //echo "The current date and universal time is ",$datum," UTC. ";
         //echo "It is day ",$doy," of this year.<br>\n";
-        //echo "The unix epoch time stamp is $tmestamp.<br>\n";
-
-        $datetime = date($dateformat, $tmestamp);
+        //echo "The unix epoch time stamp is $timestamp.<br>\n";
+        //echo date("d/m/Y H:i:s", $timestamp);
     } else {
         //echo "Unfortunately, the time server $timeserver could not be reached at this time. ";
         //echo "$timercvd[1] $timercvd[2].<br>\n";
@@ -55,6 +54,6 @@ if(!function_exists("get_server_time")) {
         show_errors();
     }
 
-    return $datetime;
+    return $timestamp;
   }
 }
