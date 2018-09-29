67a40284c9b7356ddffda122cb7f8e5230523faf
Namhyeon, Go <gnh1201@gmail.com>
2018-09-30 06:15:43 +0900

    Update webproxy.php

--- route/webproxy.php
+++ route/webproxy.php
@@ -3,9 +3,18 @@ if(!defined("_DEF_RSF_")) set_error_exit("do not allow access");
 
 loadHelper("webpagetool");
 
+$method = get_requested_value("method");
 $url = get_requested_value("url");
 
+$res_method = "get.cache";
+$res_methods = explode(".", $method);
+if(in_array("nocache", $res_methods)) {
+	$res_method = "get";
+}
+
 if(!empty($url)) {
-	$response = get_web_page($url, "get.cache");
+	$response = get_web_page($url, $res_method);
 	echo $response['content'];
 }
+
+write_common_log(sprintf("%s\t%s\t%s", get_current_datetime(), "webproxy", $url));
