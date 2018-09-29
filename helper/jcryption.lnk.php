b639d9209f97530013f161a7c95362be476a4d54
Namhyeon, Go <gnh1201@gmail.com>
2018-09-30 03:30:30 +0900

    Update jcryption.lnk.php

--- helper/jcryption.lnk.php
+++ helper/jcryption.lnk.php
@@ -1,3 +1,22 @@
 <?php
-require_once("./vendor/jCryption/sqAES.php");
-require_once("./vendor/jCryption/sJCryption.php");
+
+if(!function_exists("jcryption_load")) {
+  function jcryption_load() {
+    $required_files = array(
+      "jCryption/sqAES",
+      "jCryption/JCryption"
+    );
+    foreach($required_files as $file) {
+      $inc_file = "./vendor/" . $file . ".php";
+      if(file_exists($inc_file)) {
+        include($inc_file);
+      }
+    }
+  }
+}
+
+if(!function_exists("jcryption_get_code")) {
+  function jcryption_get_code() {
+    return "JCryption::decrypt();";
+  }
+}
