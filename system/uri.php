0c3a75bf1207a895a7066eb9525fe70b02687576
Go Namhyeon <gnh1201@gmail.com>
2018-12-31 10:41:23 +0900

    enhance xss protection

--- system/uri.php
+++ system/uri.php
@@ -65,12 +65,13 @@ if(!function_exists("read_requests")) {
 		}
 
 		// with security module
-	\if(function_exists("get_clean_xss")) {
-			foreach($requests['_GET'] as $k=>$v) {
-				if(is_string($v)) {
-					$requests['_GET'][$k] = get_clean_xss($v);
-				}
-			}
+        $protect_methods = array("_ALL", "_GET", "_POST", "_JSON");
+        if(function_exists("get_clean_xss")) {
+            foreach($protect_methods as $method) {
+                foreach($requests[$method] as $k=>$v) {
+                    $requests[$method][$k] = is_string($v) ? get_clean_xss($v) : $v;
+                }
+            }
 		}
 
 		// set alias
