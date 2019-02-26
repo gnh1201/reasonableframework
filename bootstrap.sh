30444145a961fc101edaa3f97d782d362b995b5a
Namhyeon, Go <gnh1201@gmail.com>
2019-02-26 16:24:34 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 find . -type d -exec chmod 0755 {} \;
 find . -type f -exec chmod 0644 {} \;
-cp ./storage/helloworld.htaccess.txt ./.htaccess
+cp ./storage/example/_.htaccess ./.htaccess
