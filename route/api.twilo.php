fba64d40790f5febe399e587c3b0d7b96335e758
Namhyeon, Go <gnh1201@gmail.com>
2019-04-15 10:58:16 +0900

    Update api.twilo.php

--- route/api.twilo.php
+++ route/api.twilo.php
@@ -8,3 +8,22 @@
 
 loadHelper("twilio.api");
 
+$action = get_requested_value("action");
+$from = get_requested_value("from");
+$to = get_requested_value("to");
+
+$sid = "";
+$token = "";
+
+switch($action) {
+  case "message":
+    $response = twilio_send_message($message, $from, $to, $sid, $token);
+    break;
+
+  case "voice":
+    $response = twilio_send_voice("", $from, $to, $sid, $token);
+    break;    
+}
+
+header("Content-Type: application/json");
+echo json_encode($response);
