3438a0d16e84d5b37bf8e0f68ada25429d0975f5
Namhyeon, Go <gnh1201@gmail.com>
2019-04-15 16:21:35 +0900

    Update api.mailgun.php

--- route/api.mailgun.php
+++ route/api.mailgun.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @file api.mailgun.php
+ * @date 2019-04-15
+ * @author Go Namhyeon <gnh1201@gmail.com>
+ * @brief Mailgun API controller
+ */
 
 loadHelper("mailgun.api");
 
@@ -6,3 +12,7 @@ $content = get_requested_value("content", array("_JSON", "_ALL"));
 $subject = get_requested_value("subject", array("_JSON", "_ALL"));
 $to = get_requested_value("to", array("_JSON", "_ALL"));
 
+$response = mailgun_send_message($content, $to, $subject);
+
+header("Content-Type: application/json");
+echo json_encode($response);
