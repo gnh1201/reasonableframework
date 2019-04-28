ef0fb081574e2e7004204c1242b95ac5070605fb
Namhyeon, Go <gnh1201@gmail.com>
2019-04-29 02:48:09 +0900

    Create view_mobileswitcher.php

---
+++ view/view_mobileswitcher.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <title>Checking your device</title>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <meta http-equiv="refresh" content="3;url=<?php echo $redirect_url; ?>" />
+</head>
+
+<body>
+    <h1>Checking your device</h1>
+    <p>We are looking for the best setting for your device.</p>
+    <p>Please wait 3 seconds...</p>
+</body>
+
+</html>
