a1fa93097222f408f6c4bcada834397506b36e4c
Namhyeon, Go <gnh1201@gmail.com>
2019-04-29 04:51:42 +0900

    Update view_mobileswitcher.php

--- view/view_mobileswitcher.php
+++ view/view_mobileswitcher.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ko" lang="ko">
 
 <head>
-    <title>Checking your device</title>
-    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-    <meta http-equiv="refresh" content="3;url=<?php echo $redirect_url; ?>" />
+    <title>기기 확인 중...</title>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+    <meta http-equiv="refresh" content="3;url=<?php echo $redirect_url; ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0"/>
+    <style type="text/css">
+        textarea { width: 97%; height: 100px; }
+    </style>
 </head>
 
 <body>
-    <h1>Checking your device</h1>
-    <p>We are looking for the best setting for your device.</p>
-    <p>Please wait 3 seconds...</p>
+    <h1>기기 확인 중...</h1>
+    <p>접속한 기기에 맞는 최적의 설정을 찾고 있습니다.</p>
+    <p>3초 후 이동합니다.</p>
+    <p>문제가 있을 시 아래 정보를 관리자에게 알려주세요.</p>
+    <p><textarea><?php echo $ua; ?></textarea></p>
 </body>
 
 </html>
