5c9d505728567a65666416ebe01aaba296784b52
Namhyeon, Go <gnh1201@gmail.com>
2019-04-29 03:43:13 +0900

    Update mobileswitcher.php

--- route/mobileswitcher.php
+++ route/mobileswitcher.php
@@ -1,6 +1,14 @@
 <?php
+/**
+ * @file mobileswitcher.php
+ * @date 2019-04-29
+ * @author Go Namhyeon <gnh1201@gmail.com>
+ * @brief mobile device switcher
+ */
+
 loadHelper("mobiletool");
 
+$do = get_requested_value("do");
 $from = get_requested_value("from");
 $redirect_url = get_requested_value("redirect_url");
 $dm = detect_mobile();
@@ -12,11 +20,17 @@ if($from == "pc") {
 }
 
 $data = array(
+    "action" => $action,
     "from" => $from,
     "dm" => $dm,
     "redirect_url" => get_final_link($redirect_url, array(
         "action" => "mobileswitcher",
         "dm" => $dm,
+        "do" => $do,
+        "redirect_url" => get_final_link($redirect_url, array(
+            "action" => "mobileswitcher",
+            "dm" => $dm,
+        ), false),
     )),
 );
 
