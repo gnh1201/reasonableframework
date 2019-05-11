61049a17e13591ce3d29aeedc32a7879f1396d87
Namhyeon, Go <gnh1201@gmail.com>
2019-05-11 15:31:46 +0900

    Update security.ini.php

--- storage/config/security.ini.php
+++ storage/config/security.ini.php
@@ -4,6 +4,7 @@ return <<<EOF
 masterkey=ZVScK4o3DTQsQjyr
 masteriv=qcLHsW6g11E1JEAF
 salt=H6hclwzFplRQw39C
+saltdisabled=0
 adjectives=warty,hoary,breezy,dapper,edgy,feisty,gutsy,hardy,intrepid,jaunty,karmic,lucid,maverick,natty,oneiric,precise,quantal,raring,saucy,trusty,utopic,vivid,wliy,xeniel,yakkety,zesty
 animals=warthog,hedgehog,badger,drake,eft,fawn,gibbon,heron,ibex,jackalope,koala,lynx,meerkat,narwhal,ocelot,pangolin,quetzal,ringtail,salamander,tahr,unicorn,vervet,werewolf,xerus,yak,zapus
 httpencrypt=auto
