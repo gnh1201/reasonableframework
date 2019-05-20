6171db84cbf09fef2bdb4c4d786cfc3b219aa16f
Go Namhyeon <gnh1201@gmail.com>
2019-05-20 18:51:31 +0900

    fix

--- storage/config/security.ini.php
+++ storage/config/security.ini.php
@@ -7,6 +7,6 @@ salt=H6hclwzFplRQw39C
 saltdisabled=0
 adjectives=warty,hoary,breezy,dapper,edgy,feisty,gutsy,hardy,intrepid,jaunty,karmic,lucid,maverick,natty,oneiric,precise,quantal,raring,saucy,trusty,utopic,vivid,wliy,xeniel,yakkety,zesty
 animals=warthog,hedgehog,badger,drake,eft,fawn,gibbon,heron,ibex,jackalope,koala,lynx,meerkat,narwhal,ocelot,pangolin,quetzal,ringtail,salamander,tahr,unicorn,vervet,werewolf,xerus,yak,zapus
-httpencrypt=auto
+https=auto
 hashalgos=md5,sha1,crypt,crc32,base64,sql_password
 EOF;
