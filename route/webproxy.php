b2722ec8fd74874ffaa1ddb9a7564440f05931cf
Namhyeon, Go <gnh1201@gmail.com>
2019-05-22 10:33:28 +0900

    Update webproxy.php

--- route/webproxy.php
+++ route/webproxy.php
@@ -4,6 +4,7 @@ if(!defined("_DEF_RSF_")) set_error_exit("do not allow access");
 loadHelper("webpagetool");
 
 $method = get_requested_value("method");
+$mime = get_requested_value("mime"); // example: text/html, image/jpeg
 $url = get_requested_value("url");
 
 $res_method = "get.cache";
@@ -14,7 +15,10 @@ if(in_array("nocache", $res_methods)) {
 
 if(!empty($url)) {
     $response = get_web_page($url, $res_method);
-    echo $response['content'];
+    if(!empty($mime)) {
+        header(sprintf("Content-Type: %s", $mime));
+    }
+    print_r($response['content']);
 }
 
 write_common_log(sprintf("%s\t%s\t%s", get_current_datetime(), "webproxy", $url));
