459838c4e89134fae82dd2eb4c25c8b96e5ead1b
Namhyeon, Go <gnh1201@gmail.com>
2019-05-26 02:43:28 +0900

    Update logger.php

--- system/logger.php
+++ system/logger.php
@@ -9,33 +9,35 @@
 if(!check_function_exists("write_visit_log")) {
     function write_visit_log() {
         $fw = false;
+        $msg = "";
         
         $event = get_network_event();
         if(loadHelper("networktool")) {
             if(loadHelper("rsf.format")) {
-                $data = DOC_EOL . get_rsf_encoded($event);
+                $msg = get_rsf_encoded($event);
             } else {
-                $data = DOC_EOL . json_encode(get_network_event());
+                $msg = json_encode(get_network_event());
             }
 
             $fw = append_storage_file($data, array(
                 "storage_type" => "logs",
                 "filename" => "network.log",
                 "chmod" => 0644,
+                "nl" => "<",
             ));
         }
 
         return $fw;
     }
 }
-    
+
 if(!check_function_exists("write_common_log")) {
     function write_common_log($msg) {
-        $msg = DOC_EOL . $msg;
         return append_storage_file($msg, array(
             "storage_type" => "logs",
             "filename" => "common.log",
             "chmod" => 0644,
+            "nl" => "<",
         ));
     }
 }
