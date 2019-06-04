a94e79b73e7df29147d5de0cd1c85f5c7884d8a3
Namhyeon, Go <gnh1201@gmail.com>
2019-06-04 18:52:37 +0900

    Update and rename php-ddos-shield.lnk.php to php-sec-ddos.lnk.php

--- helper/php-ddos-shield.lnk.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "DDoS protection is only for sponsors: <a href=\"https://patreon.com/catswords\">https://patreon.com/catswords</a>";
-?>
---
+++ helper/php-sec-ddos.lnk.php
@@ -0,0 +1,2 @@
+<?php
+echo "php-sec-ddos is only for sponsors: <a href=\"https://patreon.com/catswords\">https://patreon.com/catswords</a>";
