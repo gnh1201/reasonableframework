3f4467ca31618f34acd06bfc083593686e92c7d8
Namhyeon, Go <gnh1201@gmail.com>
2019-06-04 15:42:47 +0900

    Update php-ddos-shield.lnk.php

--- helper/php-ddos-shield.lnk.php
+++ helper/php-ddos-shield.lnk.php
@@ -1,3 +1,3 @@
 <?php
-echo "DDoS protection is only for sponsors: <a href="https://patreon.com/catswords">https://patreon.com/catswords</a>";
+echo "DDoS protection is only for sponsors: <a href=\"https://patreon.com/catswords\">https://patreon.com/catswords</a>";
 ?>
