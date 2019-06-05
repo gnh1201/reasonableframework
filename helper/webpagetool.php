1134d3f7a40fd6f761701fa63b10d4b0057c32a4
Namhyeon, Go <gnh1201@gmail.com>
2019-06-05 13:01:10 +0900

    Update webpagetool.php

--- helper/webpagetool.php
+++ helper/webpagetool.php
@@ -596,7 +596,7 @@ if(!check_function_exists("get_webproxy_url")) {
 if(!check_function_exists("get_web_user_agent")) {
     function get_web_user_agent($ua="") {
         if(empty($ua)) {
-            $ua = "ReasonableFramework/1.2-dev (https://github.com/gnh1201/reasonableframework)";
+            $ua = "ReasonableFramework/1.4-dev (https://github.com/gnh1201/reasonableframework)";
         } else {
             $ua = make_safe_argument($ua);
         }
