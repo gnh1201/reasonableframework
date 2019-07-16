7275126ceab07a256b8a08a4c0616d7b46cf3e32
Namhyeon, Go <gnh1201@gmail.com>
2019-07-16 16:11:25 +0900

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ define("APP_DEVELOPMENT", false); // set the status of development
 define("DOC_EOL", "\r\n"); // set the 'end of line' commonly
 define("CORS_DOMAINS", false); // common security: allow origin domains (e.g. example.org,*.example.org)
 define("PHP_FIREWALL_REQUEST_URI", strip_tags($_SERVER['REQUEST_URI'])); // advanced security
-define("PHP_FIREWALL_ACTIVATION", true); // advanced security
+define("PHP_FIREWALL_ACTIVATION", false); // advanced security
 define("PHP_DDOS_PROTECTION", false); // advanced security
 
 // check if current status is development
