8fa2039379b55ef85f5436d9c2cf55c8b6df6f0e
Namhyeon, Go <gnh1201@gmail.com>
2019-10-13 00:27:19 +0900

    Update json.format.php

--- helper/json.format.php
+++ helper/json.format.php
@@ -1,5 +1,6 @@
 <?php
-// json.format.php
+// @date 2019-10-13
+// @author Go Namhyeon <gnh1201@gmail.com>
 
 if(!check_function_exists("json_decode_ex")) {
     function json_decode_ex($data, $options=array()) {
