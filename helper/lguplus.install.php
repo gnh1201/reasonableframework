ccdc8de9d394c637b8019d0999dbebca67f586b6
Namhyeon, Go <gnh1201@gmail.com>
2019-10-13 22:42:41 +0900

    Update and rename lguplus.autoinst.php to lguplus.install.php

--- helper/lguplus.install.php
+++ helper/lguplus.install.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @file lguplus.autoinst.php
+ * @file lguplus.install.php
  * @date 2019-10-13
  * @author Go Namhyeon <gnh1201@gmail.com>
  * @brief `LGU+`or `LGUPlus` is trandmark of LGUPlus Co. Ltd.
@@ -9,14 +9,14 @@
 if(!defined("_DEF_RSF_")) set_error_exit("do not allow access");
 
 if(!check_function_exists("install_lguplus")) {
-    function install_pgkcp() {
+    function lguplus_install() {
         $response = get_web_page("https://openapi.sms.uplus.co.kr/sdkFile/php_sdk.zip");
 
         $fw = write_storage_file($response['content'], array(
             "extension" => "zip"
         ));
         @unzip($fw, get_storage_path());
-        
+
         // todo
     }
 }
