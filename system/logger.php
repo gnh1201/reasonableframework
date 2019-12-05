2eae693cb1d40e43f5370ade82e309913e62bd6d
Namhyeon, Go <gnh1201@gmail.com>
2019-12-05 19:41:58 +0900

    Update logger.php

--- system/logger.php
+++ system/logger.php
@@ -5,27 +5,32 @@
  * @author Go Namhyeon <gnh1201@gmail.com>
  * @brief Logger module for ReasonableFramework
  */
- 
+
+if(!check_function_exists("write_log_to_file")) {
+    function write_log_to_file($data, $filename) {
+        return append_storage_file($data, array(
+            "storage_type" => "logs",
+            "filename" => $filename,
+            "chmod" => 0644,
+            "nl" => "<",
+        ));
+    }
+}
+
 if(!check_function_exists("write_visit_log")) {
     function write_visit_log() {
         $fw = false;
-        $data = "";
-        
-        if(loadHelper("networktool")) {
-            $event = get_network_event();
 
+        $data = "";
+        if(loadHelper("networktool")) {
+            $nevt = get_network_event();
             if(loadHelper("catsplit.format")) {
-                $data = catsplit_encode($event);
+                $data = catsplit_encode($nevt);
             } else {
-                $data = json_encode($event);
+                $data = json_encode($nevt);
             }
 
-            $fw = append_storage_file($data, array(
-                "storage_type" => "logs",
-                "filename" => "network.log",
-                "chmod" => 0644,
-                "nl" => "<",
-            ));
+            $fw = write_log_to_file($data, "network.log");
         }
 
         return $fw;
@@ -37,12 +42,7 @@ if(!check_function_exists("write_common_log")) {
         $fw = false;
 
         $data = implode("\t", array(get_current_datetime(), $type, $msg));
-        $fw = append_storage_file($data, array(
-            "storage_type" => "logs",
-            "filename" => "common.log",
-            "chmod" => 0644,
-            "nl" => "<",
-        ));
+        $fw = write_log_to_file($data, "common.log");
 
         // send to networks
         $_networks = explode(",", $networks);
@@ -53,5 +53,14 @@ if(!check_function_exists("write_common_log")) {
         }
 
         return $fw;
+    }
+}
+
+if(!check_function_exists("write_debug_log")) {
+    function write_debug_log($msg, $type="None") {
+        if(APP_DEVELOPMENT !== false) {
+            $data = implode("\t", array(get_current_datetime(), $type, $msg));
+            return write_log_to_file($data, "debug.log");
+        }
     }
 }
