e7590171f353139a05765674eaabc18784b580bf
Namhyeon, Go <gnh1201@gmail.com>
2020-01-09 22:09:01 +0900

    Update database.php

--- system/database.php
+++ system/database.php
@@ -315,7 +315,6 @@ if(!check_function_exists("get_bind_to_sql_insert")) {
                 "getcount" => true,
                 "setwheres" => $setnotwhere
             ));
-            write_common_log($_sql, "BBBBBB");
             $_rows = exec_db_fetch_all($_sql, $_bind_K);
             foreach($_rows as $_row) {
                 $num_duplicates += intval($_row['value']);
