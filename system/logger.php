ba31880a1fecb8e6a04e14729fdc1f0059fe1a5b
Namhyeon, Go <gnh1201@gmail.com>
2020-01-23 21:44:27 +0900

    Update logger.php

--- system/logger.php
+++ system/logger.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * @file logger.php
- * @date 2018-05-27
+ * @created_on 2018-05-27
+ * @updated_on 2020-01-23
  * @author Go Namhyeon <gnh1201@gmail.com>
  * @brief Logger module for ReasonableFramework
  */
