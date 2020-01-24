f73293c138c30a7854e065cabad289834a6f1808
Namhyeon, Go <gnh1201@gmail.com>
2020-01-25 01:39:53 +0900

    Update view_orderpay.step2.pgkcp.php

--- view/view_orderpay.step2.pgkcp.php
+++ view/view_orderpay.step2.pgkcp.php
@@ -1,9 +1,9 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html>
     <head>
         <title>*** NHN KCP [AX-HUB Version] ***</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta http-equiv="Pragma" content="no-cache"> 
         <meta http-equiv="Expires" content="-1">
     </head>
