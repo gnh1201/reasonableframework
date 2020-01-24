a4a555bcef662c10c4f0770bd52c592718ddad10
Namhyeon, Go <gnh1201@gmail.com>
2020-01-25 01:39:27 +0900

    Update view_orderpay.pgkcp.php

--- view/view_orderpay.pgkcp.php
+++ view/view_orderpay.pgkcp.php
@@ -13,8 +13,8 @@ if(!defined("_DEF_RSF_")) set_error_exit("do not allow access");
 <html>
 <head>
     <title>*** NHN KCP [AX-HUB Version] ***</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta http-equiv="Pragma" content="no-cache"> 
     <meta http-equiv="Expires" content="-1">
 </head>
