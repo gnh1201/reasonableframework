afd3e5d5668979b937ae7f90381950b0a639c89a
Namhyeon, Go <gnh1201@gmail.com>
2020-02-19 14:43:06 +0900

    Create perftool.php

---
+++ helper/perftool.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file perftool.php
+ * @created_on 2020-02-19
+ * @updated_on 2020-02-19
+ * @author Go Namhyeon <gnh1201@gmail.com>
+ * @brief PerfTool helper
+ */
+
+if(!is_fn("get_cpu_idle")) {
+    function get_cpu_idle() {
+        $idle = 100.0;
+
+        if(loadHelper("exectool")) {
+            $idle = floatval(trim(exec_command("top -n 1 | grep -i Cpu\(s\) | awk '{print \$8}'")));
+        }
+
+        return $idle;
+    }
+}
