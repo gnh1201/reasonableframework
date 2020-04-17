c7ee370f4e307812bb380f6fab271176a2e4d414
Namhyeon, Go <gnh1201@gmail.com>
2020-04-17 18:08:11 +0900

    Update SSL.class.php

--- helper/SSL.class.php
+++ helper/SSL.class.php
@@ -71,3 +71,16 @@ if(!class_exists("SSL")) {
         }
     }
 }
+
+/*
+## Example
+```
+$certInfo = SSL::getSSLinfo('stackoverflow.com'); 
+echo $certInfo->validFrom .'<br>';
+echo $certInfo->validTo .'<br>';
+echo $certInfo->issuer .'<br>';
+echo $certInfo->validity .'<br>';
+echo $certInfo->validitytot .'<br>';
+echo $certInfo->crtValRemaining .'<br>';
+```
+*/
