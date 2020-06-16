36c0bf53f5dc63d4a422668736d6f3a47113187d
Namhyeon, Go <gnh1201@gmail.com>
2020-06-16 16:42:54 +0900

    Update security.ini.php

--- storage/config/security.ini.php
+++ storage/config/security.ini.php
@@ -9,7 +9,7 @@ adjectives=warty,hoary,breezy,dapper,edgy,feisty,gutsy,hardy,intrepid,jaunty,kar
 animals=warthog,hedgehog,badger,drake,eft,fawn,gibbon,heron,ibex,jackalope,koala,lynx,meerkat,narwhal,ocelot,pangolin,quetzal,ringtail,salamander,tahr,unicorn,vervet,werewolf,xerus,yak,zapus
 https=auto
 hashalgos=md5,sha1,crypt,crc32,base64,sql_password
-sandboxdisabled=0
+sandboxdisabled=1
 allowextensionsdisabled=1
 allowextensions=png,gif,jpg,jpeg,tif,xls,ppt,doc,xlsx,pptx,docx,odt,odp,ods,xlsm,tiff,pdf,xlsm,mp3,ogg,m4a,wma,wav
 EOF;
