4dc7b9f6ad3353eb3c4849dfec86588ba607d5ca
Namhyeon, Go <gnh1201@gmail.com>
2020-06-21 16:57:09 +0900

    Update logger.ini.php

--- storage/config/logger.ini.php
+++ storage/config/logger.ini.php
@@ -4,6 +4,6 @@ return <<<EOF
 rfc3164_enabled=0
 rfc3164_host=logs6.papertrailapp.com
 rfc3164_port=42986
-log_mode_visit=database
-log_rotate_time=7889238
+log_rotate_size=104857600
+log_rotate_ratio=0.9
 EOF;
