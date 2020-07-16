ef3907c5ceddeea3eda94ed9f3d7dd4c59060677
Namhyeon, Go <gnh1201@gmail.com>
2020-07-16 15:10:39 +0900

    Update webpagetool.php

--- helper/webpagetool.php
+++ helper/webpagetool.php
@@ -2,7 +2,7 @@
 /**
  * @file webpagetool.php
  * @created_on 2018-06-01
- * @updated_on 2020-04-08
+ * @updated_on 2020-07-16
  * @author Go Namhyeon <gnh1201@gmail.com>
  * @brief WebPageTool helper
  */
