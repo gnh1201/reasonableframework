de0f4289ba5bbc5e0403e03526a4e4f253416b11
Namhyeon, Go <gnh1201@gmail.com>
2021-05-22 05:30:39 +0900

    Update coupang.api.php

--- helper/coupang.api.php
+++ helper/coupang.api.php
@@ -17,8 +17,8 @@ if(!is_fn("coupang_get_signature")) {
 if(!is_fn("coupang_search_items")) {
     function coupang_search_items($keyword, $ACCESS_KEY, $SECRET_KEY) {
         $URL_PARTS = array("https://api-gateway.coupang.com", "/v2/providers/affiliate_open_api/apis/openapi/v1", "/products/search");
-
         $BASE_URL = $URL_PARTS[0] . '/' . $URL_PARTS[1];
+
         $path = $URL_PARTS[1] . '/' . $URL_PARTS[2];
 
         return get_web_json($BASE_URL . "/products/search". "get", array(
