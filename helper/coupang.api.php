caa26cd1eab991d0bc34005b8dcdd04fe4317f00
Namhyeon, Go <gnh1201@gmail.com>
2021-05-22 05:28:08 +0900

    Create coupang.api.php

---
+++ helper/coupang.api.php
@@ -0,0 +1,33 @@
+<?php
+loadHelper("webpagetool");
+
+if(!is_fn("coupang_get_signature")) {
+    function coupang_get_signature($method = "GET", $path, $ACCESS_KEY, $SECRET_KEY) {
+        $datetime = date("ymd") . 'T' . date("His") . 'Z';
+        $message = $datetime . $method . str_replace("?", "", $path);
+        $algorithm = "HmacSHA256";
+
+        $signature = hmacsha256_sign_message($message, $SECRET_KEY);
+        return "CEA algorithm=HmacSHA256, access-key=" . $ACCESS_KEY.", signed-date=" . $datetime.", signature=" . $signature;
+    }
+}
+
+if(!is_fn("coupang_search_items")) {
+    function coupang_search_items($keyword, $ACCESS_KEY, $SECRET_KEY) {
+        $URL_PARTS = array("https://api-gateway.coupang.com", "/v2/providers/affiliate_open_api/apis/openapi/v1", "/products/search");
+
+        $BASE_URL = $URL_PARTS[0] . '/' . $URL_PARTS[1];
+        $path = $URL_PARTS[1] . '/' . $URL_PARTS[2];
+
+        return get_web_json($BASE_URL . "/products/search". "get", array(
+            "headers" => array(
+                "Authorization" => coupang_get_signature("GET", $path, $ACCESS_KEY, $SECRET_KEY),
+                "data" => array(
+                    "keyword" => $keyword,
+                    "limit" => 20, 
+                    //"subId" => ""
+                )
+            )
+        ));
+    }
+}
